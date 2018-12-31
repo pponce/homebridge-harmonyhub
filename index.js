70ad896e2aa7eb4d045d5fc8992b6e06f0992738
pponce <pponce@users.noreply.github.com>
2018-12-31 10:11:39 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 var Service, Characteristic, Accessory, uuid;
 
 module.exports = function (homebridge) {
-	Service = homebridge.hap.Service;
-	Characteristic = homebridge.hap.Characteristic;
-	Accessory = homebridge.hap.Accessory;
-	uuid = homebridge.hap.uuid;
-	var exportedTypes = {
-		Service: homebridge.hap.Service,
-		Characteristic: homebridge.hap.Characteristic,
-		Accessory: homebridge.hap.Accessory,
-		PlatformAccessory: homebridge.platformAccessory,
-		uuid: homebridge.hap.uuid
-	};
-	exportedTypes.AccessoryBase = require('./lib/accessory-base')(exportedTypes);
-	exportedTypes.HubAccessoryBase = require('./lib/hub-accessory-base')(exportedTypes);
-	exportedTypes.ActivityAccessory = require('./lib/activity-accessory')(exportedTypes);
-	exportedTypes.Hub = require('./lib/hub')(exportedTypes);
-	exportedTypes.HomePlatform = require('./lib/home-platform')(exportedTypes);
+        Service = homebridge.hap.Service;
+        Characteristic = homebridge.hap.Characteristic;
+        Accessory = homebridge.hap.Accessory;
+        uuid = homebridge.hap.uuid;
+        var exportedTypes = {
+                Service: homebridge.hap.Service,
+                Characteristic: homebridge.hap.Characteristic,
+                Accessory: homebridge.hap.Accessory,
+                PlatformAccessory: homebridge.platformAccessory,
+                uuid: homebridge.hap.uuid
+        };
+        exportedTypes.AccessoryBase = require('./lib/accessory-base')(exportedTypes);
+        exportedTypes.HubAccessoryBase = require('./lib/hub-accessory-base')(exportedTypes);
+        exportedTypes.VolumeService = require('./lib/volume-service')(exportedTypes);
+        exportedTypes.ActivityAccessory = require('./lib/activity-accessory')(exportedTypes);
+        exportedTypes.Hub = require('./lib/hub')(exportedTypes);
+        exportedTypes.HomePlatform = require('./lib/home-platform')(exportedTypes);
 
-	homebridge.registerPlatform("homebridge-harmonyhub", "HarmonyHub", exportedTypes.HomePlatform, true);
+        homebridge.registerPlatform("homebridge-harmonyhub", "HarmonyHub", exportedTypes.HomePlatform, true);
 };
